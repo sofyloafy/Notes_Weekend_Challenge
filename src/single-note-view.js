12219bb1033921586a561bba3828db707a4ce3d0
Nick Butler <nwmbutler@gmail.com>
2020-06-25 15:59:45 +0100

    Step 7 tested and passing

--- src/single-note-view.js
+++ src/single-note-view.js
@@ -3,6 +3,11 @@
   function SingleNoteView(noteModel) {
     this.noteModel = noteModel; 
 }
+
+SingleNoteView.prototype.returnView = function () {
+    return `<div>${this.noteModel.text}</div>`
+}
+
 exports.SingleNoteView = SingleNoteView;
 
 })(this);
--- tests/single-note-view-test.js
+++ tests/single-note-view-test.js
@@ -1,7 +1,14 @@
 
 function constructsWithANoteModel() {
-    let list = new Note();
-    let singleView = new SingleNoteView(list);
-    expect.isTrue(singleView.noteModel instanceof Note);
+  let list = new Note();
+  let singleView = new SingleNoteView(list);
+  expect.isTrue(singleView.noteModel instanceof Note);
 }
 constructsWithANoteModel();
+
+function returnsStringOfHtml() {
+  let list = new Note("Favourite drink: BEER");
+  let singleView = new SingleNoteView(list);
+  expect.isTrue(singleView.returnView() === "<div>Favourite drink: BEER</div>")
+}
+returnsStringOfHtml();
