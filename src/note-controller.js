6cc0a33ab03cb8f1b62ba379bfc2daa34ada6ad2
Sophie <sophiebrown438@gmail.com>
2020-06-26 16:23:37 +0100

    Might be working to display note text

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,8 +1,10 @@
 
 (function (exports) {
 
-    function NoteController(notelist) {
-        this.noteListModel = notelist;
+    function NoteController(Notelist) {
+        this.noteListModel = Notelist;
+        this.noteListModel.addNote('It is FRIDAY');
+
         this.view = new NoteListView(this.noteListModel)
     }
 
@@ -14,9 +16,16 @@
     exports.NoteController = NoteController;
 })(this);
 
-
+    window.addEventListener('DOMContentLoaded', function(event){
+        var list = new NoteList();
+        var controller = new NoteController(list);
+        controller.getHTML();
 
     window.addEventListener("hashchange", function(event) {
         var id = window.location.hash.split('#')[1]
-     
- 
+        var note = controller.noteListModel.returnNote()[id]
+        var singleNoteView = new SingleNoteView(note)
+        var html = singleNoteView.returnView()
+        this.document.getElementById('app').innerHTML = html
+    })
+    });
